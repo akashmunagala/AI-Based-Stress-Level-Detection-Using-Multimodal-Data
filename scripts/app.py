163e82dc89ac671ede5eb2a2671697c3bd47f870
Akash Munagala <akashmunagala7@gmail.com>
2026-05-15 19:02:27 +0530

    Fixed Streamlit image width

--- scripts/app.py
+++ scripts/app.py
@@ -269,13 +269,11 @@ if page == "Home":
 
     # 🔥 CENTER IMAGE (CLEAN SaaS STYLE)
     col1, col2, col3 = st.columns([1, 2, 1])
-
-    with col2:
-        st.image(
-            "https://healthindustrytrends.com/wp-content/uploads/2024/11/revolutionizing-remote-patient-care-with-iot-technology.png",
-            
-            width='stretch'
-        )
+with col2:
+    st.image(
+        "https://healthindustrytrends.com/wp-content/uploads/2024/11/revolutionizing-remote-patient-care-with-iot-technology.png",
+        use_column_width=True
+    )
 
     st.markdown("---")
 
